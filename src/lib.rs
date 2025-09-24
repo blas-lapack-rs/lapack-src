6b621c32c7dd0553fafbdbbfd3f241d39a59ccfb
geo-ant <54497890+geo-ant@users.noreply.github.com>
2025-09-24 19:40:26 +0200

    simplify mkl linkage options

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,21 @@
 //!
 //! ```toml
 //! [dependencies]
-//! lapack-src = { version = "0.11", features = ["accelerate"] }
-//! lapack-src = { version = "0.11", features = ["intel-mkl"] }
-//! lapack-src = { version = "0.11", features = ["netlib"] }
-//! lapack-src = { version = "0.11", features = ["openblas"] }
-//! lapack-src = { version = "0.11", features = ["r"] }
+//! lapack-src = { version = "0.12", features = ["accelerate"] }
+//! lapack-src = { version = "0.12", features = ["intel-mkl"] }
+//! lapack-src = { version = "0.12", features = ["netlib"] }
+//! lapack-src = { version = "0.12", features = ["openblas"] }
+//! lapack-src = { version = "0.12", features = ["r"] }
 //! ```
+//! ### Configuring MKL
+//!
+//! When the `intel-mkl` feature is selected, then the parallel version of
+//! MKL using OpenMP is _statically_ linked. To link the sequential version
+//! use the `intel-mkl-seq` feature. In both cases, the
+//! [LP64 interface](https://www.intel.com/content/www/us/en/docs/onemkl/developer-guide-linux/2023-0/using-the-ilp64-interface-vs-lp64-interface.html)
+//! is linked. If other linkage options for MKL are desired, omit `lapack-src`
+//! as a dependency and use the [`intel-mkl-src`](https://crates.io/crates/intel-mkl-src)
+//! crate directly with the appropriate feature flags.
 //!
 //! [architecture]: https://blas-lapack-rs.github.io/architecture
 //! [lapack]: https://en.wikipedia.org/wiki/LAPACK
@@ -37,17 +46,7 @@
 #[cfg(feature = "accelerate")]
 extern crate accelerate_src as raw;
 
-#[cfg(any(
-    feature = "intel-mkl",
-    feature = "intel-mkl-static-lp64-iomp",
-    feature = "intel-mkl-static-lp64-seq",
-    feature = "intel-mkl-static-ilp64-iomp",
-    feature = "intel-mkl-static-ilp64-seq",
-    feature = "intel-mkl-dynamic-lp64-iomp",
-    feature = "intel-mkl-dynamic-lp64-seq",
-    feature = "intel-mkl-dynamic-ilp64-iomp",
-    feature = "intel-mkl-dynamic-ilp64-seq"
-))]
+#[cfg(any(feature = "intel-mkl", feature = "intel-mkl-seq",))]
 extern crate intel_mkl_src as raw;
 
 #[cfg(feature = "netlib")]
